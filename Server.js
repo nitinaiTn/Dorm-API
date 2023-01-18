ec98de3c40b6b83748b3ad564a8c85e95d6d3c1e
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 00:37:50 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+const express = require("express")
+const app = express()
+const con = require("./config/db.js")
+
+// connecting route to database
+app.use(function(req, res, next) {
+    req.con = con
+    next()
+  })
+
+// parsing body request
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// include router
+const userRouter = require("./routes/user")
+
+// routing
+app.use("/userdata", userRouter)
+
+// starting server
+app.listen(3000, function() {
+  console.log("server listening on port 3000")
+})
