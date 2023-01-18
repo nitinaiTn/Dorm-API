cbaa10d864a288421d7f6654d20b8fe799d2752a
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 04:39:01 +0700

    Update loginModel.js

--- models/loginModel.js
+++ models/loginModel.js
@@ -1,1 +1,19 @@
-console.log("H")
+const mysql = require("../config/db");
+
+let Login = function (user) {
+  this.username = user.username;
+  this.password = user.password;
+};
+
+Login.findByEmail = function (username, result) {
+  mysql.query("SELECT * FROM users WHERE username = ?", username, function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
+module.exports = Login;
