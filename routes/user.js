51c2db17933a7647bdcde2164879072cee35b239
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 00:53:33 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.get('/', userController.index)
-router.get('/', userController.create)
+router.get('/create', userController.create)
 router.post('/', userController.store)
 
 module.exports = router
