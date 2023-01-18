c42fd302c94493b89b7dac76ef249caf18bba269
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 02:54:44 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-const UserRoute = require("./routes/user.routes");
+const UserRoute = require("./routes/user");
 
 app.use("/api/user", UserRoute);
 
