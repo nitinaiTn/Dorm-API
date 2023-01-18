cda91b599143341575677c34463619b36956eab0
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 03:24:33 +0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -41,4 +41,30 @@ User.create = function (newEmp, result) {
   });
 };
 
+User.delete = function (id, result) {
+  mysql.query("DELETE FROM user WHERE user_id = ?", [id], function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
+User.update = function (id, user, result) {
+  mysql.query(
+    "UPDATE user SET user_id=?,user_name=?,user_email=?,user_password=? WHERE id = ?",
+    [user.user_id, user.user_name, user.user_email, user.user_password, id],
+    function (err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
+
 module.exports = User;
