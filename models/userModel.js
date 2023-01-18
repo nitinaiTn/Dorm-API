63cbd972573e21a86971512c1100184e1fffb5cf
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 02:50:59 +0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -30,36 +30,3 @@ User.findById = function (id, result) {
 };
 
 module.exports = User;
-
-// module.exports = {
-//     get: function(con, callback) {
-//       con.query("SELECT * FROM Users", callback)
-//     },
-  
-//     getById: function(con, id, callback) {
-//       con.query(`SELECT * FROM Users WHERE user_id = ${id}`, callback)
-//     },
-  
-//     create: function(con, data, callback) {
-//       con.query(
-//         `INSERT INTO Users SET 
-//         name = '${data.name}', 
-//         password = '${data.password}'`,
-//         callback
-//       )
-//     },
-  
-//     update: function(con, data, id, callback) {
-//       con.query(
-//         `UPDATE Users SET 
-//         name = '${data.name}', 
-//         password = '${data.password}' 
-//         WHERE user_id = ${id}`,
-//         callback
-//       )
-//     },
-  
-//     destroy: function(con, id, callback) {
-//       con.query(`DELETE FROM Users WHERE user_id = ${id}`, callback)
-//     }
-//   }
