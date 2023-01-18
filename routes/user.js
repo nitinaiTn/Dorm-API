3fb8f1be40eebfddcc01f1563d18c4a723298921
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 00:34:57 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,9 @@
-console.log('JJ')
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+
+router.get('/', userController.index)
+router.get('/', userController.create)
+router.post('/', userController.store)
+
+module.exports = router
