d9d933b77388d0a0a5330bdb1de8dac921980564
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 01:02:57 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,22 +1,37 @@
 const express = require("express")
+const cors = require('cors')
 const app = express()
-const con = require("./config/db.js")
+const connection = require("./config/db.js")
 
-// connecting route to database
-app.use(function(req, res, next) {
-    req.con = con
-    next()
-  })
 
-// parsing body request
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.get('/',(req,res) =>{
+  res.send('hello world')
+})
 
-// include router
-const userRouter = require("./routes/user")
+app.get('/user', (req,res)=>{
+  connection.query(
+    'SELECT * FROM Users',
+    function(err,result,fields){
+      res.send(result)
+    }
+  )
+})
+// // connecting route to database
+// app.use(function(req, res, next) {
+//     req.con = con
+//     next()
+//   })
 
-// routing
-app.use("/userdata", userRouter)
+// // parsing body request
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// // include router
+// const userRouter = require("./routes/user")
+
+// // routing
+// app.use("/userdata", userRouter)
 
 // starting server
 app.listen(3000, function() {
