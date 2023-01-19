5218b7bc08e7d31cdc5fe8703dc8d74dd53a5053
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 14:04:17 +0700

    Create postRoute.js

---
+++ routes/postRoute.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const postController = require('../controllers/postController')
+
+router.get('/', postController.findAll)
+router.get('/:user_id', postController.findByUserId)
+router.post('/', postController.create)
+router.put('/:post_id', postController.update)
+router.delete('/:post_id', postController.delete)
+
+module.exports = router
