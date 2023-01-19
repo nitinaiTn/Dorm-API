eed43372b67eb702a9f23b65df522c9ec22ec6ec
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 14:01:17 +0700

    Create postModel.js

---
+++ models/postModel.js
@@ -0,0 +1,74 @@
+const mysql = require('../config/db')
+
+let Post = function (post) {
+  this.post_id = post.post_id;
+  this.user_id = post.user_id;
+  this.post_title = post.post_title;
+  this.post_text = post.post_text;
+  this.date_created = post.date_created;
+};
+
+Post.findAll = function (result) {
+  mysql.query("Select * from Posts", function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
+      console.log("Posts : ", res);
+      result(null, res);
+    }
+  });
+};
+
+Post.findByUserId = function (user_id, result) {
+  mysql.query("Select * from Posts where user_id = ? ", user_id, function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
+Post.create = function (newPost, result) {
+  mysql.query("INSERT INTO Posts set ?",newPost, function (err, res) {
+    if (err) {
+    console.log("error: ", err);
+    result(err, null);
+    } else {
+    console.log(res.insertId);
+    result(null, res.insertId);
+    }
+    });
+    };
+    
+    Post.delete = function (post_id, result) {
+    mysql.query("DELETE FROM Posts WHERE post_id = ?", [post_id], function (err, res) {
+    if (err) {
+    console.log("error: ", err);
+    result(null, err);
+    } else {
+    result(null, res);
+    }
+    });
+    };
+    
+    Post.update = function (post_id, post, result) {
+    mysql.query(
+    "UPDATE Posts SET user_id=?,post_title=?,post_text=?,date_created=? WHERE post_id = ?",
+    [post.user_id, post.post_title, post.post_text, post.date_created, post_id],
+    function (err, res) {
+    if (err) {
+    console.log("error: ", err);
+    result(null, err);
+    } else {
+    result(null, res);
+    }
+    }
+    );
+    };
+    
+module.exports = Post;
+    
+    
