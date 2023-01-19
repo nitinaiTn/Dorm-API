189fb0ef101a07fb9b2280c03a490ddb252e8e7b
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 10:14:14 +0700

    Create maintenanceRoute.js

---
+++ routes/maintenanceRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const maintenanceController = require("../controllers/MaintenanceController");
+
+// Get all maintenance requests
+router.get("/", maintenanceController.findAll);
+
+// Get a single maintenance request by ID
+router.get("/:id", maintenanceController.findById);
+
+// Create a new maintenance request
+router.post("/", maintenanceController.create);
+
+// Delete a maintenance request by ID
+router.delete("/:id", maintenanceController.delete);
+
+// Update a maintenance request by ID
+router.put("/:id", maintenanceController.update);
+
+module.exports = router;
