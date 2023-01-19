9c1605e531f5cbfdac399c6ca6bdcd04bd7ad465
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 13:35:09 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -11,10 +11,12 @@ app.use(cors())
 const loginRoute = require('./routes/loginRoute')
 const UserRoute = require("./routes/userRoute")
 const MaintenanceRoute = require('./routes/maintenanceRoute')
+const LeaseRoute = require('./routes/leaseRoute')
 
 app.use('/api/login', loginRoute)
 app.use("/api/user", UserRoute)
 app.use('/api/maintenance', MaintenanceRoute)
+app.use('/api/lease', LeaseRoute)
 
 app.get("/", (req, res) => {
   res.json({
