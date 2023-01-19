5c25434ffe0db3d20a252e9dbebda54b7266ea04
nitinaiTn <nitinai.tn@gmail.com>
2023-01-19 13:57:15 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -13,12 +13,14 @@ const UserRoute = require("./routes/userRoute")
 const MaintenanceRoute = require('./routes/maintenanceRoute')
 const LeaseRoute = require('./routes/leaseRoute')
 const UtilityRoute = require('./routes/utilityRoute')
+const PropertyRoute = require('./routes/propertyRoute')
 
 app.use('/api/login', loginRoute)
 app.use("/api/user", UserRoute)
 app.use('/api/maintenance', MaintenanceRoute)
 app.use('/api/lease', LeaseRoute)
 app.use('/api/utility', UtilityRoute)
+app.use('/api/property', PropertyRoute)
 
 app.get("/", (req, res) => {
   res.json({
