b8cc7f914ffadb9d258624980595fd0a1b1836c9
nitinaiTn <nitinai.tn@gmail.com>
2023-02-01 19:25:19 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -12,27 +12,25 @@ app.use(cors())
 // const UserRoute = require("./routes/userRoute")
 // const MaintenanceRoute = require('./routes/maintenanceRoute')
 // const LeaseRoute = require('./routes/leaseRoute')
-// const UtilityRoute = require('./routes/utilityRoute')
-// const PropertyRoute = require('./routes/propertyRoute')
-// const PostRoute = require('./routes/postRoute')
-// const CommentRoute = require('./routes/commentRoute')
-
-// app.use('/api/login', loginRoute)
-// app.use("/api/user", UserRoute)
-// app.use('/api/maintenance', MaintenanceRoute)
-// app.use('/api/lease', LeaseRoute)
-// app.use('/api/utility', UtilityRoute)
-// app.use('/api/property', PropertyRoute)
-// app.use('/api/post', PostRoute)
-// app.use('/api/comment', CommentRoute)
-
-
+const UtilityRoute = require('./routes/utilityRoute')
+const PropertyRoute = require('./routes/propertyRoute')
+const PostRoute = require('./routes/postRoute')
+const CommentRoute = require('./routes/commentRoute')
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
   });
 });
+
+// app.use('/api/login', loginRoute)
+// app.use("/api/user", UserRoute)
+// app.use('/api/maintenance', MaintenanceRoute)
+// app.use('/api/lease', LeaseRoute)
+app.use('/api/utility', UtilityRoute)
+app.use('/api/property', PropertyRoute)
+app.use('/api/post', PostRoute)
+app.use('/api/comment', CommentRoute)
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("server listening on port 3000")
