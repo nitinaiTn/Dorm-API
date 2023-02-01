1c15c2245062c7a67e84f5a5155608f7aa7ebcbc
nitinaiTn <nitinai.tn@gmail.com>
2023-02-01 19:46:14 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 const express = require("express")
-const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mysql = require('./config/db')
 
+const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
