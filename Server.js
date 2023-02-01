ee465df0e7ccaf48769088cc6db434f071e5b286
nitinaiTn <nitinai.tn@gmail.com>
2023-02-01 19:48:49 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,9 @@ const mysql = require('./config/db')
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 const loginRoute = require('./routes/loginRoute')
 const UserRoute = require("./routes/userRoute")
