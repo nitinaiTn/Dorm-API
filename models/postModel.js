27ae3b11a3abb6b4799509cef97245162cea5edd
nitinaiTn <nitinai.tn@gmail.com>
2023-02-03 02:49:54 +0700

    Update postModel.js

--- models/postModel.js
+++ models/postModel.js
@@ -55,13 +55,13 @@ Post.create = function (newPost, result) {
     };
     
 Post.update = function (post_id, post, result) {
+  res.send(post)
   mysql.query("UPDATE Posts SET user_id=?,post_title=?,post_text=?,date_created=? WHERE post_id = ?",
     [post.user_id, post.post_title, post.post_text, post.date_created, post_id], function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(null, err);}
       else {
-        console.log("succes form model")
         result(null, res);
       }
     }
