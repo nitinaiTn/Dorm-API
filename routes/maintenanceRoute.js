b8cda364274c8836ae4c134b710162db27a4a4a6
nitinaiTn <nitinai.tn@gmail.com>
2023-02-09 11:52:58 +0700

    Fix Maintenance

--- routes/maintenanceRoute.js
+++ routes/maintenanceRoute.js
@@ -5,16 +5,16 @@ const maintenanceController = require("../controllers/maintenanceController")
 // Get all maintenance requests
 router.get("/", maintenanceController.findAll);
 
-// // Get a single maintenance request by ID
-// router.get("/:id", maintenanceController.findById);
+// Get a single maintenance request by ID
+router.get("/:id", maintenanceController.findById);
 
-// // Create a new maintenance request
-// router.post("/", maintenanceController.create);
+// Create a new maintenance request
+router.post("/", maintenanceController.create);
 
-// // Delete a maintenance request by ID
-// router.delete("/:id", maintenanceController.delete);
+// Delete a maintenance request by ID
+router.delete("/:id", maintenanceController.delete);
 
-// // Update a maintenance request by ID
-// router.put("/:id", maintenanceController.update);
+// Update a maintenance request by ID
+router.put("/:id", maintenanceController.update);
 
 module.exports = router
