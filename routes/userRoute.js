39ba0c47fe1e547a3b12786d3d07b45208550670
nitinaiTn <nitinai.tn@gmail.com>
2023-02-09 08:21:24 +0700

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,7 @@ const UserController = require('../controllers/userController')
 
 router.get("/", UserController.findAll);
 router.get("/:id", UserController.findById);
-router.get("/getJoin/", UserController.findById);
+router.get("/getJoin/", UserController.findAll);
 router.post("/", UserController.create)
 router.delete("/:id", UserController.delete);
 router.put('/:id', UserController.update);
