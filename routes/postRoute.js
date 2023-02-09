dbdcc204edaf7ac9ec5c2662057cc05b29195810
nitinaiTn <nitinai.tn@gmail.com>
2023-02-09 08:30:25 +0700

    Update postRoute.js

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -4,10 +4,11 @@ const postController = require('../controllers/postController')
 
 router.get('/', postController.findAll)
 router.get('/:user_id', postController.findByUserId)
+router.get('/getJoin', postController.findAll)
 router.post('/', postController.create)
 router.put('/:post_id', postController.update)
 router.delete('/:post_id', postController.delete)
-router.get('/getJoin', postController.getJoin)
+
 
 
 module.exports = router
