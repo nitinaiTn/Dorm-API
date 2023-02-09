6be1cc582f3ef42e8c4e0386c5c98630266c364e
nitinaiTn <nitinai.tn@gmail.com>
2023-02-09 11:27:36 +0700

    Update maintenanceController.js

--- controllers/maintenanceController.js
+++ controllers/maintenanceController.js
@@ -16,33 +16,33 @@ exports.findById = function (req, res) {
   });
 };
 
-exports.create = function (req, res) {
-  const new_request = new MaintenanceRequest(req.body);
-  //handles null error
-  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
-    res
-      .status(400)
-      .send({ error: true, message: "Please provide all required field" });
-  } else {
-    MaintenanceRequest.create(new_request, function (err, request) {
-      if (err) res.send(err);
-      res.json({
-        data: request,
-      });
-    });
-  }
-};
+// exports.create = function (req, res) {
+//   const new_request = new MaintenanceRequest(req.body);
+//   //handles null error
+//   if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+//     res
+//       .status(400)
+//       .send({ error: true, message: "Please provide all required field" });
+//   } else {
+//     MaintenanceRequest.create(new_request, function (err, request) {
+//       if (err) res.send(err);
+//       res.json({
+//         data: request,
+//       });
+//     });
+//   }
+// };
 
-exports.delete = function (req, res) {
-  MaintenanceRequest.delete(req.params.id, function (err, request) {
-    if (err) res.send(err);
-    res.json({ message: "Maintenance Request successfully deleted" });
-  });
-};
+// exports.delete = function (req, res) {
+//   MaintenanceRequest.delete(req.params.id, function (err, request) {
+//     if (err) res.send(err);
+//     res.json({ message: "Maintenance Request successfully deleted" });
+//   });
+// };
 
-exports.update = function (req, res) {
-  MaintenanceRequest.update(req.params.id, new MaintenanceRequest(req.body), function (err, request) {
-    if (err) res.send(err);
-    res.json({ message: "Maintenance Request successfully updated" });
-  });
-};
+// exports.update = function (req, res) {
+//   MaintenanceRequest.update(req.params.id, new MaintenanceRequest(req.body), function (err, request) {
+//     if (err) res.send(err);
+//     res.json({ message: "Maintenance Request successfully updated" });
+//   });
+// };
