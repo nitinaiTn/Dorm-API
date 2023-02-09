0670f65c3c6c36d98e9e6e930835929febc05dcc
nitinaiTn <nitinai.tn@gmail.com>
2023-02-09 11:46:02 +0700

    Update maintenanceRoute.js

--- routes/maintenanceRoute.js
+++ routes/maintenanceRoute.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const maintenanceController = require("../controllers/MaintenanceController");
+const express = require("express")
+const router = express.Router()
+const maintenanceController = require("../controllers/MaintenanceController")
 
 // Get all maintenance requests
 router.get("/", maintenanceController.findAll);
