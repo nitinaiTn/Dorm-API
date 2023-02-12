0bafe09a2a5f6def9641208d05e7f98d593fd7c6
nitinaiTn <nitinai.tn@gmail.com>
2023-02-12 16:40:14 +0700

    Update sendEmailScript.js

--- utils/sendEmailScript.js
+++ utils/sendEmailScript.js
@@ -1,41 +1,41 @@
 const nodemailer = require('nodemailer');
 const cron = require('cron');
 
-async function sendEmail() {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.example.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'you@example.com',
-      pass: 'password'
-    }
-  });
+// async function sendEmail() {
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.example.com',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: 'you@example.com',
+//       pass: 'password'
+//     }
+//   });
 
-  const today = new Date();
-  if (today.getDate() === 25) {
-    const mailOptions = {
-      from: 'you@example.com',
-      to: 'recipient@example.com',
-      subject: 'Daily Email',
-      text: 'This is a daily email sent on the 25th day of the month.'
-    };
+//   const today = new Date();
+//   if (today.getDate() === 25) {
+//     const mailOptions = {
+//       from: 'you@example.com',
+//       to: 'recipient@example.com',
+//       subject: 'Daily Email',
+//       text: 'This is a daily email sent on the 25th day of the month.'
+//     };
 
-    try {
-      const info = await transporter.sendMail(mailOptions);
-      console.log(`Email sent: ${info.response}`);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-}
+//     try {
+//       const info = await transporter.sendMail(mailOptions);
+//       console.log(`Email sent: ${info.response}`);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// }
 
-const cronJob = new cron.CronJob({
-  cronTime: '0 0 * * 25',
-  onTick: sendEmail,
-  timeZone: 'Asia/Bangkok'
-});
+// const cronJob = new cron.CronJob({
+//   cronTime: '0 0 * * 25',
+//   onTick: sendEmail,
+//   timeZone: 'Asia/Bangkok'
+// });
 
 // cronJob.start();
 
-module.exports = cronJob
+// module.exports = cronJob
