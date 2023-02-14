2a088181a73fdc9fd08d261307c5c0c5df54ab89
nitinaiTn <nitinai.tn@gmail.com>
2023-02-14 21:55:02 +0700

    Update billController.js

---
+++ controllers/billController.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require('../config/db')
+
+app.get('/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const query = `
+      SELECT * FROM Bills
+      WHERE user_id = ${userId} AND bill_date >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
+    `;
+
+    mysql.query(query, (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+
+module.exports = router
