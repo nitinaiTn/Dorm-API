f19a6026be26fa0219fb69f6364354857282a8ea
nitinaiTn <nitinai.tn@gmail.com>
2023-02-23 09:35:15 +0700

    Update billController.js

--- controllers/billController.js
+++ controllers/billController.js
@@ -19,10 +19,11 @@ router.get('/:userId', (req, res) => {
 router.get('/', (req, res) => {
   const userId = req.params.userId;
   const query = `
-      SELECT * FROM Bills
-      WHERE user_id = ${userId} 
+      SELECT * FROM Bills b
+      join Rooms r on b.user_id = r.user_id
+      join User u on b.user_id = u.user_id
     `;
-
+      // WHERE user_id = ${userId} 
   mysql.query(query, (err, results) => {
     if (err) throw err;
     res.json(results);
